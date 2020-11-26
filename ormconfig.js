9700e98ec6943adf35447ce3e2bfa8c914f8b6a3
trecky <pedrobealves@gmail.com>
2020-11-26 20:34:30 -0300

    Removed yarn lock

--- ormconfig.js
+++ ormconfig.js
@@ -1,9 +1,9 @@
 module.exports = {
   "type": "postgres",
-  "url": "postgres://ytrtpcuvgaxgfv:8b621075082644b38d96acd5ccb3a1a3855ae44fb84be4ab3b3dfc165c2be989@ec2-54-84-98-18.compute-1.amazonaws.com:5432/d2374lgr0jk5t8",
-  "entities": ["./src/modules/**/infra/typeorm/entities/*.ts"],
-  "migrations": ["./src/modules/**/infra/typeorm/migrations/*.ts"],
+  "url": process.env.DATABASE_URL || "postgres://postgres:postgres@localhost:5432/blogmil_dashboard",
+  "entities": ["./dist/modules/**/infra/typeorm/entities/*.js"],
+  "migrations": ["./dist/modules/**/infra/typeorm/migrations/*.js"],
   "cli": {
-    "migrationsDir": "./src/modules/**/infra/typeorm/migrations/"
+    "migrationsDir": "./dist/modules/**/infra/typeorm/migrations/"
   }
 }
