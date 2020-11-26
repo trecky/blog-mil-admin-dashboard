70fd6d2f99ee1976184b83e79ca0b38c42b387e8
trecky <pedrobealves@gmail.com>
2020-11-26 18:31:34 -0300

    Prepare deploy

--- packages/server/src/shared/infra/http/server.ts
+++ packages/server/src/shared/infra/http/server.ts
@@ -34,6 +34,6 @@ app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   });
 });
 
-app.listen(3333, () => {
+app.listen(process.env.port || 3333, () => {
   console.log('Server started');
 });
